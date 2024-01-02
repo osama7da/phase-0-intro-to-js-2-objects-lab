507393dadf69b3d0952d7c0f25c3923881f20e44
Osama daoud <osamadaoud66@gmail.com>
2024-01-03 00:30:08 +0300

    compleated assignment

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your solution in this file!
+const employee ={ 
+    name : "osama",
+    streetAddress: 'salam street',
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    const newEmployee = {...employee};
+    newEmployee.name = "sam"; 
+    newEmployee[key]=value ; 
+    return newEmployee
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee.name = "Sam";
+    employee[key] = value;
+    return employee;
+  }
+   
+  function deleteFromEmployeeByKey (employee,key){
+    const newEmployee = {...employee};
+    delete newEmployee[key]
+    return newEmployee ; 
+
+  }
+ 
+  function destructivelyDeleteFromEmployeeByKey(employee,key){
+    delete employee[key];
+    return employee ; 
+
+  }
